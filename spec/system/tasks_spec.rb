266f8b01a40fc325c5acfc11efa20b8200377049
koalamask <yaiag04@gmail.com>
2018-12-29 17:52:14 +0900

    Add test codes(it does not work yet)

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,19 +1,38 @@
-require 'rspec-rails'
+require 'rails_helper'
 
-RSpec. do 
-    describe ''
-    context ''
+describe 'タスク管理機能', type: :system do
+    describe '一覧表示機能' do
         before do
-            'ユーザAを作成する'
-            '作成者がユーザAであるタスクをひとつ作成する'
         end
-    
-        it do 
-            'ログイン画面に繊維'
-            'ログインフォームにメールアドレス入力'
-            'ログインフォームにパスワード入力'
-            'ログインボタン押下'
-            'ログインに成功'し'タスクがひとつ存在する'状態を期待する
-        end
+        
+        context '最初のタスクが存在しているとき'    
+            before do
+                FactoryBot.create(:task)
+                visit tasks_path
+            end
 
+            it '最初のタスクが表示される' do 
+                expect(:page).to have_content '最初のタスク'
+            end
+        end
+    end
 end
+
+# describe 'タスク管理機能', type: :system do
+    #     describe '一覧表示機能' do    
+    #         before do
+    #             user_a = FactoryBot.create(:user)
+    #             FactoryBot.create(:task, name: "最初のタスク", user: user_a)
+    #         end
+        
+    #     context 'ユーザAがログインしているとき'
+    #         before do
+    #             visit login_path
+    #             fill_in 'メールアドレス',  with 'a@example.com'
+    #             fill_in 'パスワード', with 'password'
+    #             click_button 'ログインする'
+    #         end
+        
+    #         it 'ユーザAが作成したタスクが表示される' do 
+    #             # 作成済みのタスク名が表示される
+    #         end
