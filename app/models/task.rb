94b51664373c9f50805e51a823f0285f17fbba15
koalamask <yaiag04@gmail.com>
2019-01-01 11:31:30 +0900

    #7 add validation for deadline(allow today or later)

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,5 +3,12 @@ class Task < ApplicationRecord
     validates :name, length: { maximum: 30 }
     validates :status, presence: true
     # FIXME: 「今日以降の日付であること」という制限を設ける
-    # validates :deadline, Date.today 
+    validate :deadline_is_today_or_later
+
+    def deadline_is_today_or_later
+        if deadline.present? && deadline < Date.today
+            errors.add(:deadline, '昨日以前の日付は締切日に設定できません')
+        end
+    end
+
 end
