ebdc7e1578857a057b78fa80cbdbe4f62dc73622
koalamask <yaiag04@gmail.com>
2019-01-05 17:01:11 +0900

    #9 Add sort direction indicator to table head

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,7 @@ module ApplicationHelper
         title ||= column.titleize
         css_class = (column == sort_column) ? "current #{sort_direction}" : nil
         direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
-        link_to title, {:sort=> column, :direction => direction} , {:class => css_class}
+        triangle = (direction == "asc") ? "▲" : "▼"
+        link_to title + triangle, {:sort=> column, :direction => direction} , {:class => css_class}
     end
 end
