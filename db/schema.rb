b84d23547f3f74d0c7753fef013c572f8d9c6e88
koalamask <yaiag04@gmail.com>
2019-01-05 23:06:59 +0900

    #10 Add index to name and status column in tasks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_05_091143) do
+ActiveRecord::Schema.define(version: 2019_01_05_125945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_01_05_091143) do
     t.integer "priority"
     t.string "status", default: "0", null: false
     t.date "deadline"
+    t.index ["name", "status"], name: "index_tasks_on_name_and_status"
   end
 
 end
