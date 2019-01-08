fc675cd02947f8770b5368dca1743aef021d0319
koalamask <yaiag04@gmail.com>
2019-01-09 08:37:23 +0900

    #16 Add tasks/index for admininstrator

---
+++ app/controllers/admin/tasks_controller.rb
@@ -0,0 +1,15 @@
+class Admin::TasksController < ApplicationController
+  def index
+    @tasks = Task.all.order(sort_column + ' ' + sort_direction).search(params[:search]).page(params[:page]).includes(:user)
+  end
+
+  private 
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
+  end
+
+  def sort_column
+    Task.column_names.include?(params[:sort]) ? params[:sort] : "name"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :admin do
     resources :users
+    get '/tasks', to: 'tasks#index'
   end
 
 
