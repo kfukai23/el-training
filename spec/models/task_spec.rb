d968414bc6aa71c44fddd7150363cd29dd2c1ce2
koalamask <yaiag04@gmail.com>
2019-01-18 23:44:28 +0900

    #7 Test passed(deny invalid record without status or user_id)

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,31 +4,52 @@ RSpec.describe Task, type: :model do
   describe 'モデルテスト' do
     before do
       admin = FactoryBot.create(:user, admin: true)
-      # user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
     end
 
     context '' do
-        it 'name, description, priority, status, deadline, user_idが揃っていたら保存されること' do
-          user = User.create(
-            name: 'ユーザA',
-            email: 'a@example.com',
-            password: 'password'
-          )
-          task = user.tasks.new(
-            name: '最初のタスク',
-            description: '最初のタスクのdescriptionです',
-            priority: 0,
-            status: '未着手',
-            deadline: Date.today + 1.days,
-          )
-          expect(task).to be_valid
-        end
+      it 'name, description, priority, status, deadline, user_idが揃っていたら保存されること' do
+        user = User.create(
+          name: 'ユーザA',
+          email: 'a@example.com',
+          password: 'password'
+        )
+        task = user.tasks.new(
+          name: '最初のタスク',
+          description: '最初のタスクのdescriptionです',
+          priority: 0,
+          status: '未着手',
+          deadline: Date.today + 1.days,
+        )
+        expect(task).to be_valid
+      end
 
-        it 'ステータスがなければ無効であること' do
-        end
+      it 'ステータスがなければ無効であること' do
+        user = User.create(
+          name: 'ユーザA',
+          email: 'a@example.com',
+          password: 'password'
+        )
+        task = user.tasks.new(
+          name: '最初のタスク',
+          description: '最初のタスクのdescriptionです',
+          priority: 0,
+          status: nil,
+          deadline: Date.today + 1.days,
+        )
+        expect(task).not_to be_valid
+      end
 
-        it 'ユーザidがなければ無効であるｋとおと' do
-        end
+      it 'ユーザidがなければ無効であること' do
+        task = Task.new(
+          name: '最初のタスク',
+          description: '最初のタスクのdescriptionです',
+          priority: 0,
+          status: '未着手',
+          deadline: Date.today + 1.days,
+          user_id: nil
+        )
+        expect(task).not_to be_valid
+      end
     end
   end
 end
