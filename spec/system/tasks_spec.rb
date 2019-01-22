ffaba8fc5802965839034a09c0ab1d55ec132a84
koalamask <yaiag04@gmail.com>
2019-01-23 01:04:02 +0900

    Add test code for search function(and modify search form labels) #46

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
+# FIXME: 必ず共通化する
+
 describe 'タスク管理機能', type: :system do
     describe '一覧表示機能' do    
         before do
@@ -103,6 +105,84 @@ describe 'タスク管理機能', type: :system do
                     task_names = all('.name').map(&:text)
                     expect(task_names).to eq %w(1番目 2番目 3番目) 
                 end
+            end
+        end
+    end
+
+    describe '一覧表示機能' do    
+        before do
+            admin = FactoryBot.create(:user, admin: true)
+            user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
+            FactoryBot.create(:task, name: "first", user: user_a, priority: 2, status: "未着手")
+            FactoryBot.create(:task, name: "second", user: user_a, priority: 1, status: "着手中")
+            FactoryBot.create(:task, name: "third", user: user_a, priority: 0, status: "完了")
+        end
+    
+        context 'ユーザAがログインしているとき' do
+            before do
+                visit login_path
+                fill_in 'メールアドレス',  with: 'a@example.com'
+                fill_in 'パスワード', with: 'password'
+                click_button 'ログインする'
+
+                fill_in 'Name', with: 'first'
+                click_button 'Search'
+            end
+        
+            it 'ユーザAが作成したタスクが優先順位の降順に表示される' do 
+                expect(page).to have_content "first"
+            end
+        end
+    end
+
+    describe '一覧表示機能' do    
+        before do
+            admin = FactoryBot.create(:user, admin: true)
+            user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
+            FactoryBot.create(:task, name: "first", user: user_a, priority: 2, status: "未着手")
+            FactoryBot.create(:task, name: "second", user: user_a, priority: 1, status: "着手中")
+            FactoryBot.create(:task, name: "third", user: user_a, priority: 0, status: "完了")
+        end
+    
+        context 'ユーザAがログインしているとき' do
+            before do
+                visit login_path
+                fill_in 'メールアドレス',  with: 'a@example.com'
+                fill_in 'パスワード', with: 'password'
+                click_button 'ログインする'
+
+                select '高', from: 'Priority'
+                click_button 'Search'
+            end
+        
+            it 'ユーザAが作成したタスクが優先順位の降順に表示される' do 
+                expect(page).to have_content "first"
+            end
+        end
+    end
+
+    describe '一覧表示機能' do    
+        before do
+            admin = FactoryBot.create(:user, admin: true)
+            user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
+            FactoryBot.create(:task, name: "first", user: user_a, priority: 2, status: "未着手")
+            FactoryBot.create(:task, name: "second", user: user_a, priority: 1, status: "着手中")
+            FactoryBot.create(:task, name: "third", user: user_a, priority: 0, status: "完了")
+        end
+    
+        context 'ユーザAがログインしているとき' do
+            before do
+                visit login_path
+                fill_in 'メールアドレス',  with: 'a@example.com'
+                fill_in 'パスワード', with: 'password'
+                click_button 'ログインする'
+
+                select '未着手', from: 'Status'
+                click_button 'Search'
+            end
+        
+            it 'ユーザAが作成したタスクが優先順位の降順に表示される' do 
+                expect(page).to have_content "first"
             end
         end
     end
