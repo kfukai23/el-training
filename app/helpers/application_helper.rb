edc5361cf32bbbeba0e14b25216b30e46e2e6bfc
koalamask <yaiag04@gmail.com>
2019-02-11 17:52:12 +0900

    Add helper for render search condition in index.html(on the way)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,27 @@ module ApplicationHelper
             link_to title, {:sort=> column, :direction => direction} , {:class => css_class}
         end
     end
+
+    # FIXME: 検索結果を表示している場合は検索条件をviewに返すように
+    # def search_condition
+    #     if request.fullpath.include?("commit=Search")
+    #         binding.pry
+    #         origin = request.fullpath.split('&')
+    #         con = []
+    #         origin.each do |o|
+    #             con << { key: o.split("=").first, value: o.split("=").second}
+    #         end
+
+    #         res = []        
+    #         con.each do |c|
+    #             if c[:value] != nil
+    #                 res << c
+    #             end
+    #         end
+    #         res
+    #     else
+    #         "タスク一覧"
+    #     end
+    # end
+
 end
