75d7ad1cc831381b603580b9f20cafb776e79c1a
kaitofu <yaiag04@gmail.com>
2019-02-24 19:28:36 +0900

    Refine tests already exists

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -10,29 +10,24 @@ describe 'タスク管理機能', type: :system do
 		end
 
 		describe 'ログイン機能' do
+			before do
+				FactoryBot.create(:task, name: "最初のタスク", user: user_a)
+				visit login_path
+				fill_in 'メールアドレス',  with: login_user.email
+				fill_in 'パスワード', with: login_user.password
+				click_button 'ログインする'
+			end
+			
 			context 'ユーザAがログインしているとき' do
-				before do
-					FactoryBot.create(:task, name: "最初のタスク", user: user_a)
-					visit login_path
-					fill_in 'メールアドレス',  with: 'a@example.com'
-					fill_in 'パスワード', with: 'password'
-					click_button 'ログインする'
-				end
+				let(:login_user) { user_a }
 		
 				it 'ユーザAが作成したタスクが表示される' do 
 					expect(page).to have_content '最初のタスク'
 				end
 			end
-
+			
 			context 'ユーザBがログインしているとき' do
 				let(:login_user) { user_b }
-				before do
-					FactoryBot.create(:task, name: "最初のタスク", user: user_a)
-					visit login_path
-					fill_in 'メールアドレス',  with: login_user.email
-					fill_in 'パスワード', with: login_user.password
-					click_button 'ログインする'
-				end
 
 				it 'ユーザAが作成したタスクが表示されない' do 
 					expect(page).not_to have_content '最初のタスク'
@@ -41,7 +36,7 @@ describe 'タスク管理機能', type: :system do
 		end
 		
 		describe '並べ替え機能' do 
-			context 'ユーザAがログインしているとき' do
+			context 'ログイン直後' do
 				before do
 					FactoryBot.create(:task, name: "1番目", user: user_a, created_at: Time.current + 2.days )
 					FactoryBot.create(:task, name: "2番目", user: user_a, created_at: Time.current + 1.days )
@@ -53,7 +48,7 @@ describe 'タスク管理機能', type: :system do
 					click_button 'ログインする'
 				end
 		
-				it 'ユーザAが作成したタスクが作成日時の降順に表示される' do 
+				it 'タスクが作成日時の降順に表示される' do 
 					within '.table' do
 						task_names = all('.name').map(&:text)
 						expect(task_names).to eq %w(1番目 2番目 3番目) 
@@ -61,7 +56,7 @@ describe 'タスク管理機能', type: :system do
 				end
 			end
 
-			context 'ユーザAがログインしているとき' do
+			context '見出し「期限」をクリックしたとき' do
 				before do
 					FactoryBot.create(:task, name: "1番目", user: user_a, deadline: Date.today )
 					FactoryBot.create(:task, name: "2番目", user: user_a, deadline: Date.today + 1.days )
@@ -74,26 +69,21 @@ describe 'タスク管理機能', type: :system do
 					click_on '期限'
 				end
 		
-				it 'ユーザAが作成したタスクが期限の昇順に表示される' do 
+				it 'タスクが期限の昇順に表示される' do 
 					within '.table' do
 						task_deadlines = all('.deadline').map(&:text)
-						expect_deadlines = []
-						expect_deadlines << Date.today.to_s
-						expect_deadlines << Date.today.succ.to_s
-						expect_deadlines << Date.today.succ.succ.to_s
+						expect_deadlines = [Date.today.to_s, Date.today.succ.to_s, Date.today.succ.succ.to_s]
 						expect(task_deadlines).to eq expect_deadlines 
 					end
 				end
 			end
-		end
 
-		describe 'タスク検索機能' do
-			context 'ユーザAがログインしているとき' do
+			context '見出し「優先度」をクリックしたとき' do
 				before do
 					FactoryBot.create(:task, name: "1番目", user: user_a, priority: 2)
 					FactoryBot.create(:task, name: "2番目", user: user_a, priority: 1)
 					FactoryBot.create(:task, name: "3番目", user: user_a, priority: 0)
-
+	
 					visit login_path
 					fill_in 'メールアドレス',  with: 'a@example.com'
 					fill_in 'パスワード', with: 'password'
@@ -101,14 +91,14 @@ describe 'タスク管理機能', type: :system do
 					click_on '優先度'
 				end
 		
-				it 'ユーザAが作成したタスクが優先順位の昇順に表示される' do 
+				it 'タスクが優先順位の昇順に表示される' do 
 					within '.table' do
 						task_names = all('.name').map(&:text)
 						expect(task_names).to eq %w(3番目 2番目 1番目) 
 					end
 				end
-
-				it 'ユーザAが作成したタスクが優先順位の降順に表示される' do 
+	
+				it 'もう一度クリックするとタスクが優先順位の降順に表示される' do 
 					click_on '優先度'
 					within '.table' do
 						task_names = all('.name').map(&:text)
@@ -116,7 +106,9 @@ describe 'タスク管理機能', type: :system do
 					end
 				end
 			end
-	
+		end
+
+		describe 'タスク検索機能' do
 			context 'ユーザAがログインしているとき' do
 				before do
 					FactoryBot.create(:task, name: "first", user: user_a, priority: 2, status: "未着手")
@@ -150,7 +142,7 @@ describe 'タスク管理機能', type: :system do
 					click_button 'Search'
 				end
 				
-				it 'ユーザAが作成したタスクが優先順位の降順に表示される' do 
+				it 'ユーザAが作成したタスクが進捗状況の降順に表示される' do 
 					expect(page).to have_content "first"
 				end
 			end
