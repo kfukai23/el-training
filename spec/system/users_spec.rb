501af63ec9b116665766e2bd289a67167c418a28
kaitofu <yaiag04@gmail.com>
2019-02-25 23:10:06 +0900

    Add test(common user can't access pages for admin user)

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 describe 'ユーザー機能', type: :system do
-  let(:admin_user_a){ FactoryBot.create(:user, name: 'admin_user', email: 'admin_user_a@example.com', admin: true) }
+  let!(:admin_user_a){ FactoryBot.create(:user, name: 'admin_user', email: 'admin_user_a@example.com', admin: true) }
+  let!(:user_a){ FactoryBot.create(:user, name: 'test_user_a', email: 'test_user_a@example.com', admin: false) }
 
   describe '管理者機能' do
     before do
@@ -16,18 +17,14 @@ describe 'ユーザー機能', type: :system do
 
       context '管理者ユーザーが自分ひとりのとき' do
         it '管理者権限を外せないこと' do
-          # 管理者ページに行く
           visit edit_admin_user_path(admin_user_a)
-          # 管理者権限チェックを外す
           uncheck 'form-label-input'
-          # 登録ボタンを押す
           click_button '登録する'
           expect(page).to have_content '管理者ユーザーが0人になるため変更できません'
         end
 
         it '削除ができないこと' do
           visit admin_user_path(admin_user_a)
-          # 登録ボタンを押す
           accept_confirm { click_on '削除' }
           expect(page).to have_content '管理者ユーザーが0人になるため変更できません'
         end
@@ -35,17 +32,14 @@ describe 'ユーザー機能', type: :system do
     end
       
     describe '管理者メニュー' do
+      let(:login_user) { user_a }
+      
       context '管理者以外のユーザーがアクセスしたとき' do
         it '管理者メニューを表示できないこと' do
-          #FIXME
+          visit admin_users_path
+          expect(page).to have_content 'お探しのページは見つかりませんでした。'
         end
       end
-
-      context '管理者以外のユーザーがCRUDを試みたとき' do
-        it 'エラーになること' do
-          #FIXME        
-        end
-      end
-    end
+    end   
   end
 end
