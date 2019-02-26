f832a1157287d88f2aea6d1338356b7810874a56
kaitofu <yaiag04@gmail.com>
2019-02-26 15:05:04 +0900

    Add test(validate label name max characters)

--- spec/models/label_spec.rb
+++ spec/models/label_spec.rb
@@ -2,11 +2,24 @@ require 'rails_helper'
 
 RSpec.describe Label, type: :model do
   describe 'モデルテスト' do
-    context '' do
-      it 'ラベル名が10文字なら有効であること' do
-      end
+    let(:user) { FactoryBot.create(:user)}
+    let(:label) { Label.new(name: name, user_id: user.id)}
+    
+    before do
+      admin = FactoryBot.create(:user, admin: true)
+    end
 
-      it 'ラベル名が11文字なら無効であること' do
+    context 'ラベル名が10文字なら有効であること' do
+      let(:name) { '1234567890' }
+      it '' do
+        expect(label).to be_valid
+      end
+    end
+
+    context 'ラベル名が11文字なら無効であること' do
+      let(:name) { '12345678901' }
+      it '' do
+        expect(label).not_to be_valid
       end
     end
   end
