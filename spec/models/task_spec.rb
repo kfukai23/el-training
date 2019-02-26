72995c78c0cc758a474b5be443e2dbb48224b0aa
kaitofu <yaiag04@gmail.com>
2019-02-26 14:52:16 +0900

    Refine with let(:task)

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -8,69 +8,52 @@ RSpec.describe Task, type: :model do
       admin = FactoryBot.create(:user, admin: true)
     end
 
-    context '' do
-      it 'name, description, priority, status, deadline, user_idが揃っていたら保存されること' do
-        task = user.tasks.new(
-          name: '最初のタスク',
-          description: '最初のタスクのdescriptionです',
-          priority: 0,
-          status: '未着手',
-          deadline: Date.today + 1.days,
-        )
+    context 'name, description, priority, status, deadline, user_idが揃っていたら保存されること' do
+      let(:task) { FactoryBot.create(:task) }
+      it '' do
         expect(task).to be_valid
       end
-
-      it 'ステータスがなければ無効であること' do
-        task = user.tasks.new(
-          name: '最初のタスク',
-          description: '最初のタスクのdescriptionです',
-          priority: 0,
-          status: nil,
-          deadline: Date.today + 1.days,
-        )
+    end
+    
+    context 'ステータスがなければ無効であること' do
+      let(:task){ FactoryBot.build(:task, status: nil) }
+      it '' do
         expect(task).not_to be_valid
       end
+    end
 
-      it 'ユーザidがなければ無効であること' do
-        task = Task.new(
-          name: '最初のタスク',
-          description: '最初のタスクのdescriptionです',
-          priority: 0,
-          status: '未着手',
-          deadline: Date.today + 1.days,
-          user_id: nil
-        )
+    context 'ユーザidがなければ無効であること' do
+      let(:task){ FactoryBot.build(:task, user_id: nil) }
+      it '' do
         expect(task).not_to be_valid
       end
+    end
 
-      it '名前が31文字を超えると無効であること' do
-        task = user.tasks.new(
-          name: '1234567890123456789012345678901',
-          description: '最初のタスクのdescriptionです',
-          priority: 0,
-          status: '未着手',
-          deadline: Date.today + 1.days,
-        )
+    context '名前が31文字を超えると無効であること' do
+      let(:task){ FactoryBot.build(:task, name: '1234567890123456789012345678901') }
+      it '' do
         expect(task).not_to be_valid
       end
+    end
 
-      it '名前が30文字ならば有効であること' do
-        #FIXME
+    context '名前が30文字ならば有効であること' do
+      let(:task){ FactoryBot.build(:task, name: '123456789012345678901234567890') }
+      it '' do
+        expect(task).to be_valid
       end
+    end
 
-      it '期限が昨日以前だと無効であること' do
-        task = user.tasks.new(
-          name: '最初のタスク',
-          description: '最初のタスクのdescriptionです',
-          priority: 0,
-          status: '未着手',
-          deadline: Date.today - 1.days,
-        )
+    context '期限が昨日以前だと無効であること' do
+      let(:task) { FactoryBot.build(:task, deadline: Date.today - 1.days) }
+      it '' do
         expect(task).not_to be_valid
       end
+    end
 
-      it '期限が今日なら有効であること' do
-        #FIXME
+    context '期限が今日ならだと有効であること' do
+      let(:task) { FactoryBot.build(:task, deadline: Date.today) }
+      it '' do
+        expect(task).to be_valid
       end
     end
   end
