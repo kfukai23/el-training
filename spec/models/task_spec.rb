3e4613d547cd05c1d1472f207c06cb056f4db78c
kaitofu <yaiag04@gmail.com>
2019-02-26 14:55:24 +0900

    Refine with subject { task }

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   describe 'モデルテスト' do
     let(:user) { User.create(name: 'ユーザA', email: 'a@example.com', password: 'password') }
+    subject{ task }
 
     before do
       admin = FactoryBot.create(:user, admin: true)
@@ -10,51 +11,37 @@ RSpec.describe Task, type: :model do
 
     context 'name, description, priority, status, deadline, user_idが揃っていたら保存されること' do
       let(:task) { FactoryBot.create(:task) }
-      it '' do
-        expect(task).to be_valid
-      end
+      it { is_expected.to be_valid }
     end
     
     context 'ステータスがなければ無効であること' do
       let(:task){ FactoryBot.build(:task, status: nil) }
-      it '' do
-        expect(task).not_to be_valid
-      end
+      it { is_expected.not_to be_valid }
     end
 
     context 'ユーザidがなければ無効であること' do
       let(:task){ FactoryBot.build(:task, user_id: nil) }
-      it '' do
-        expect(task).not_to be_valid
-      end
+      it { is_expected.not_to be_valid }
     end
 
     context '名前が31文字を超えると無効であること' do
       let(:task){ FactoryBot.build(:task, name: '1234567890123456789012345678901') }
-      it '' do
-        expect(task).not_to be_valid
-      end
+      it { is_expected.not_to be_valid }
     end
 
     context '名前が30文字ならば有効であること' do
       let(:task){ FactoryBot.build(:task, name: '123456789012345678901234567890') }
-      it '' do
-        expect(task).to be_valid
-      end
+      it { is_expected.to be_valid }
     end
 
     context '期限が昨日以前だと無効であること' do
       let(:task) { FactoryBot.build(:task, deadline: Date.today - 1.days) }
-      it '' do
-        expect(task).not_to be_valid
-      end
+      it { is_expected.not_to be_valid }
     end
 
-    context '期限が今日ならだと有効であること' do
+    context '期限が今日なら有効であること' do
       let(:task) { FactoryBot.build(:task, deadline: Date.today) }
-      it '' do
-        expect(task).to be_valid
-      end
+      it { is_expected.to be_valid }
     end
   end
 end
