00291f50988d87dc0c539629735123d1b226f1cf
kaitofu <yaiag04@gmail.com>
2019-02-27 13:50:55 +0900

    Fix test(about default order of tasks)

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -38,45 +38,45 @@ describe 'タスク管理機能', type: :system do
 		describe '並べ替え機能' do 
 			context 'ログイン直後' do
 				before do
-					FactoryBot.create(:task, name: "1番目", user: user_a, created_at: Time.current + 2.days )
-					FactoryBot.create(:task, name: "2番目", user: user_a, created_at: Time.current + 1.days )
-					FactoryBot.create(:task, name: "3番目", user: user_a, created_at: Time.current)
-
-					visit login_path
-					fill_in 'メールアドレス',  with: 'a@example.com'
-					fill_in 'パスワード', with: 'password'
-					click_button 'ログインする'
-				end
-		
-				it 'タスクが作成日時の降順に表示される' do 
-					within '.table' do
-						task_names = all('.name').map(&:text)
-						expect(task_names).to eq %w(1番目 2番目 3番目) 
-					end
-				end
-			end
-
-			context '見出し「期限」をクリックしたとき' do
-				before do
 					FactoryBot.create(:task, name: "1番目", user: user_a, deadline: Date.today )
 					FactoryBot.create(:task, name: "2番目", user: user_a, deadline: Date.today + 1.days )
 					FactoryBot.create(:task, name: "3番目", user: user_a, deadline: Date.today + 2.days )
-
+	
 					visit login_path
 					fill_in 'メールアドレス',  with: 'a@example.com'
 					fill_in 'パスワード', with: 'password'
 					click_button 'ログインする'
-					click_on '期限'
 				end
-		
-				it 'タスクが期限の昇順に表示される' do 
+				
+				it 'タスクが完了期限の昇順に表示される' do 
 					within '.table' do
 						task_deadlines = all('.deadline').map(&:text)
-						expect_deadlines = [Date.today.to_s, Date.today.succ.to_s, Date.today.succ.succ.to_s]
+						expect_deadlines = [Date.today.to_s, (Date.today + 1.days).to_s, (Date.today + 2.days).to_s]
 						expect(task_deadlines).to eq expect_deadlines 
 					end
 				end
 			end
+			
+			# context '見出し「作成日時」をクリックしたとき' do
+			# 	before do
+			# 		FactoryBot.create(:task, name: "1番目", user: user_a, created_at: Time.current + 2.days )
+			# 		FactoryBot.create(:task, name: "2番目", user: user_a, created_at: Time.current + 1.days )
+			# 		FactoryBot.create(:task, name: "3番目", user: user_a, created_at: Time.current)
+	
+			# 		visit login_path
+			# 		fill_in 'メールアドレス',  with: 'a@example.com'
+			# 		fill_in 'パスワード', with: 'password'
+			# 		click_button 'ログインする'
+			# 		click_on ''
+			# 	end
+		
+			# 	it 'タスクが作成日時の降順に表示される' do 
+			# 		within '.table' do
+			# 			task_names = all('.name').map(&:text)
+			# 			expect(task_names).to eq %w(1番目 2番目 3番目) 
+			# 		end
+			# 	end
+			# end
 
 			context '見出し「優先度」をクリックしたとき' do
 				before do
