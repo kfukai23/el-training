2170956cebc3908baa2c21fbda9a4ec88d620281
kaitofu <yaiag04@gmail.com>
2019-04-04 19:31:58 +0900

    Fix variable name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,8 +5,8 @@ module ApplicationHelper
     direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
     
     if request.fullpath.include?(column)
-      triangle = (direction == "asc") ? "▼" : "▲" 
-      link_to title + triangle, {:sort=> column, :direction => direction} , {:class => css_class}
+      sort_direction_display = (direction == "asc") ? "▼" : "▲" 
+      link_to title + sort_direction_display, {:sort=> column, :direction => direction} , {:class => css_class}
     else
       link_to title, {:sort=> column, :direction => direction} , {:class => css_class}
     end
@@ -14,19 +14,19 @@ module ApplicationHelper
   
   def search_condition
     if request.fullpath.include?("commit=Search")
-      search_condition_hash = [] 
+      search_conditions_hash = [] 
 
       request.fullpath.split('&')[1..4].each do |condition|
-        search_condition_hash << { key: condition.split("=").first, value: condition.split("=").second }
+        search_conditions_hash << { key: condition.split("=").first, value: condition.split("=").second }
       end
 
-      res = ''
-      search_condition_hash.each do |condition| 
+      response_text = ""
+      search_conditions_hash.each do |condition| 
         if condition[:value] != nil 
-          res << convert(condition[:key]) + "=\"#{URI.decode(condition[:value])}\"  " 
+          response_text << convert(condition[:key]) + "=\"#{URI.decode(condition[:value])}\"  " 
         end
       end
-      res == "" ? res : res << "での検索結果："
+      response_text == "" ? "" : response_text << "での検索結果："
     end
   end
 
